acf668ab8e0873489a81bc8329b79484e5a2c006
Gokul K Anil <gokulkdh@gmail.com>
2023-12-13 22:30:23 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,39 @@ function App() {
   return (
     <div style={{ backgroundColor: '#09090F',minHeight: 'auto',width:'auto', padding: '0',margin:'0',boxSizing:'border-box'}}>
       <div style={{padding:"10vw",marginLeft:'10vw'}} >
-        <h1 style={{color:"#FFFFFF",fontSize:'35px',fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'}}>Privacy Policy</h1>
+        <h1 style={{color:"#FFFFFF",fontSize:'35px',fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'}}>Terms of Service</h1>
         <p className='para'>Date: September 27, 2023</p>
-        <h2 className='title'>Introduction</h2>
-        <p className='para'>Welcome to [Your Company Name] ("we," "our," or "us"). At [Your Company Name], we are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy explains how we collect, use, and protect your personal data when you use our website, products, or services. By accessing or using our services, you consent to the practices described in this Privacy Policy.</p>
-        <h2 className='title'>Information We Collect</h2>
-        <p className='para'>We may collect various types of information, including:</p>
+        <h2 className='title'>Acceptance of Terms</h2>
+        <p className='para'>Welcome to Worko, a task management platform provided by [Your Company Name] ("we," "our," or "us"). By accessing or using Worko, you agree to comply with and be bound by the following Terms of Service. If you do not agree to these terms, please refrain from using Worko.</p>
+        <h2 className='title'>Use of Worko</h2>
         <ul className='para'>
-        <li><b>Personal Information:</b> Such as your name, email address, and other identifying information you provide when registering or contacting us.</li>
-        <li><b>Usage Data:</b>  Information about how you interact with our website, products, and services, including IP addresses, device and browser information, and access times.</li>
-        <li><b>Cookies:</b>We use cookies to collect information about your preferences, settings, and browsing patterns. You can manage cookie preferences in your browser settings.</li>
-        <li><b>Third-party Information:</b> Information we may receive from third-party sources or services, which may include social media platforms.</li>        
+        <li>You agree to use Worko for lawful purposes and in compliance with all applicable laws and regulations.</li>
+        <li>You may not engage in any behavior that disrupts or interferes with the operation of Worko.</li>
+        <li>We reserve the right to suspend or terminate your access to Worko for violations of these terms.</li>
         </ul>
-        <h2 className='title'>How We Use Your Information</h2>
-        <p className='para'>We use your information for various purposes, including:</p>
+        <h2 className='title'>Privacy Policy</h2>
+        <p className='para'>Your use of Worko is also governed by our Privacy Policy, which outlines how we collect, use, and protect your personal information. You can review our Privacy Policy [here](insert link).</p>
+        <h2 className='title'>User Responsibilities</h2>
         <ul className='para'>
-        <li>Providing and improving our products and services.</li>
-        <li>Responding to your inquiries and support requests.</li>
-        <li>Sending you promotional materials and updates (if you opt-in).</li>
-        <li>Complying with legal obligations and protecting our rights.</li>
+        <li>You are responsible for the security of your Worko account and any activities that occur under your account.</li>
+        <li>You may not share your Worko login credentials or access another user's account.</li>
+        <li>You may not use Worko in a manner that infringes upon the rights of others or violates any applicable laws.</li>
         </ul>
-        <h2 className='title'>Data Security</h2>
-        <p className='para'>We take the security of your personal information seriously and employ appropriate technical and organizational measures to safeguard it. However, no method of online transmission or storage is 100% secure, and we cannot guarantee absolute security.</p>
+        <h2 className='title'>Intellectual Property</h2>
+        <p className='para'>Worko contains proprietary content, trademarks, and intellectual property that are protected by copyright and other laws. You may not use Worko's content without our explicit consent.</p>
         <h2 className='title'>Third-party Links</h2>
-        <p className='para'>Our website may contain links to third-party sites or services not operated by us. We are not responsible for the privacy practices of such third parties. We encourage you to review their privacy policies.</p>
-        <h2 className='title'>Your Choices</h2>
-        <p className='para'>You can update or delete your personal information by contacting us. You can also opt-out of receiving promotional communications from us.</p>
-        <h2 className='title'>Changes to This Policy</h2>
-        <p className='para'>We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will post the updated Privacy Policy on our website.</p>
+        <p className='para'>Worko may contain links to third-party websites or services not operated by us. We are not responsible for the content, privacy practices, or terms of such third-party sites.</p>
+        <h2 className='title'>Termination</h2>
+        <p className='para'>We reserve the right to terminate or suspend your access to Worko without prior notice for any violation of these terms or for any other reason we deem appropriate.</p>
+        <h2 className='title'>Disclaimers</h2>
+        <ul className='para'>
+          <li>Worko is provided "as is" without warranties of any kind, whether expressed or implied.</li>
+          <li>We are not liable for any damages or losses resulting from your use of Worko.</li>
+        </ul>
+        <h2 className='title'>Governing Law</h2>
+        <p className='para'>These Terms of Service are governed by the laws of Worko.</p>
+        <h2 className='title'>Changes to This Agreement</h2>
+        <p className='para'>We may update these Terms of Service from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will post the updated Terms of Service on our website.</p>
       </div>
       
       
