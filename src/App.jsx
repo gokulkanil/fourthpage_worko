7e054db7799d42bd1a83b9e5de35fab1cb359fad
Gokul K Anil <gokulkdh@gmail.com>
2023-12-13 20:00:39 +0530

    first1

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,34 @@ function App() {
   return (
     <div style={{ backgroundColor: '#09090F',minHeight: 'auto',width:'auto', padding: '0',margin:'0',boxSizing:'border-box'}}>
       <div style={{padding:"10vw",marginLeft:'10vw'}} >
-        <h1 style={{color:"#FFFFFF",fontSize:'35px',fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'}}>Data Processing Agreement</h1>
-        <p className='para'>Date: September 27, 2023<br/><br/>
-        This Data Processing Agreement (the "Agreement") is entered into by and between:<div/>     
-        <b>Your Company Name</b>, a company registered under the laws of <b>Your Country</b>, having its registered office at <b>Your Address</b> ("Processor")</p>
-        <p className='para'><b>Customer Name</b>, a company registered under the laws of <b>Customer's Country</b>, having its registered office at <b>Customer's Address</b> ("Controller").</p>
-        <p className='para'>(hereinafter collectively referred to as the "Parties").</p>
-        <h2 className='title'>Background</h2>
-        <ol className='para'>
-        <li><b>The Controller</b> is a user of Worko, a task management platform, and processes personal data of individuals ("Data Subjects") within the platform.</li>
-        <li><b>The Processor</b> provides the Worko platform to the Controller and processes personal data on behalf of the Controller as part of its services.</li>
-        <li>The Parties desire to ensure that personal data is processed in compliance with applicable data protection laws, including but not limited to the General Data Protection Regulation (GDPR).</li>
-        </ol>
-        <h2 className='title'>Definitions</h2>
+        <h1 style={{color:"#FFFFFF",fontSize:'35px',fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'}}>Privacy Policy</h1>
+        <p className='para'>Date: September 27, 2023</p>
+        <h2 className='title'>Introduction</h2>
+        <p className='para'>Welcome to [Your Company Name] ("we," "our," or "us"). At [Your Company Name], we are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy explains how we collect, use, and protect your personal data when you use our website, products, or services. By accessing or using our services, you consent to the practices described in this Privacy Policy.</p>
+        <h2 className='title'>Information We Collect</h2>
+        <p className='para'>We may collect various types of information, including:</p>
         <ul className='para'>
-        <li>"GDPR" means the General Data Protection Regulation (EU) 2016/679.</li>
-        <li>"Personal Data" shall have the meaning as defined in the GDPR.</li>
-        <li>"Processing" shall have the meaning as defined in the GDPR.</li>
+        <li><b>Personal Information:</b> Such as your name, email address, and other identifying information you provide when registering or contacting us.</li>
+        <li><b>Usage Data:</b>  Information about how you interact with our website, products, and services, including IP addresses, device and browser information, and access times.</li>
+        <li><b>Cookies:</b>We use cookies to collect information about your preferences, settings, and browsing patterns. You can manage cookie preferences in your browser settings.</li>
+        <li><b>Third-party Information:</b> Information we may receive from third-party sources or services, which may include social media platforms.</li>        
         </ul>
-        <h2 className='title'>Data Processing Terms</h2>
-        <ol className='para'>
-          <li><h3>Scope and purpose of Processing</h3>The Processor shall process Personal Data on behalf of the Controller solely for the purpose of providing the services specified in the applicable agreement between the Parties.</li>
-          <li><h3>Data Subject Rights</h3>The Processor shall provide assistance to the Controller in responding to Data Subjects' requests to exercise their rights under the GDPR, including access, rectification, erasure, and data portability.</li>
-          <li><h3>Data Security</h3>The Processor shall implement appropriate technical and organizational measures to ensure the security and confidentiality of Personal Data. These measures shall comply with the requirements of the GDPR.</li>
-          <li><h3>Data Transfers</h3>Any transfer of Personal Data outside the European Economic Area (EEA) shall comply with the requirements of the GDPR and shall only be performed with the Controller's prior consent.</li>
-          <li><h3>Sub-processors</h3>The Processor may engage sub-processors to perform specific processing activities. The Processor shall ensure that such sub-processors are bound by data protection obligations consistent with the terms of this Agreement.</li>
-          <li><h3>Confidentiality</h3>The Processor shall ensure that all personnel processing Personal Data are subject to confidentiality obligations.</li>
-          <li><h3>Data Breach Notification</h3>The Processor shall promptly notify the Controller in case of a Personal Data breach.</li>
-        </ol>
-        <h2 className='title'>Duration and Termination</h2>
-        <p className='para'>This Agreement shall remain in effect for the duration of the processing activities as specified in the underlying agreement between the Parties. Upon termination or completion of processing, the Processor shall delete or return all Personal Data to the Controller, as specified in the underlying agreement.</p>
-        <h2 className='title'>Governing Law</h2>
-        <p className='para'>This Agreement shall be governed by and construed in accordance with the laws of Your Country.</p>
-        <h2 className='title'>Execution</h2>
-        <p className='para'>This Agreement may be executed in any number of counterparts, each of which shall be deemed an original and all of which together shall constitute one and the same instrument.</p>
+        <h2 className='title'>How We Use Your Information</h2>
+        <p className='para'>We use your information for various purposes, including:</p>
+        <ul className='para'>
+        <li>Providing and improving our products and services.</li>
+        <li>Responding to your inquiries and support requests.</li>
+        <li>Sending you promotional materials and updates (if you opt-in).</li>
+        <li>Complying with legal obligations and protecting our rights.</li>
+        </ul>
+        <h2 className='title'>Data Security</h2>
+        <p className='para'>We take the security of your personal information seriously and employ appropriate technical and organizational measures to safeguard it. However, no method of online transmission or storage is 100% secure, and we cannot guarantee absolute security.</p>
+        <h2 className='title'>Third-party Links</h2>
+        <p className='para'>Our website may contain links to third-party sites or services not operated by us. We are not responsible for the privacy practices of such third parties. We encourage you to review their privacy policies.</p>
+        <h2 className='title'>Your Choices</h2>
+        <p className='para'>You can update or delete your personal information by contacting us. You can also opt-out of receiving promotional communications from us.</p>
+        <h2 className='title'>Changes to This Policy</h2>
+        <p className='para'>We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will post the updated Privacy Policy on our website.</p>
       </div>
       
       
