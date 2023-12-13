afaa3341ef88d469f8b378d0409bb3f7d72d4490
Gokul K Anil <gokulkdh@gmail.com>
2023-12-13 22:58:45 +0530

    first4

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,34 @@ function App() {
   return (
     <div style={{ backgroundColor: '#09090F',minHeight: 'auto',width:'auto', padding: '0',margin:'0',boxSizing:'border-box'}}>
       <div style={{padding:"10vw",marginLeft:'10vw'}} >
-        <h1 style={{color:"#FFFFFF",fontSize:'35px',fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'}}>Terms of Service</h1>
+        <h1 style={{color:"#FFFFFF",fontSize:'35px',fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'}}>Report a Vulnerability</h1>
         <p className='para'>Date: September 27, 2023</p>
-        <h2 className='title'>Acceptance of Terms</h2>
-        <p className='para'>Welcome to Worko, a task management platform provided by [Your Company Name] ("we," "our," or "us"). By accessing or using Worko, you agree to comply with and be bound by the following Terms of Service. If you do not agree to these terms, please refrain from using Worko.</p>
-        <h2 className='title'>Use of Worko</h2>
+        <h2 className='title'>Reporting a Vulnerability</h2>
+        <p className='para'>To report a security vulnerability, please email our security team at <a href='mailto:weareworko@gmail.com'>weareworko@gmail.com</a><div/>Please include the following information in your report:</p>
+        <ol className='para'>
+        <li><b>Description:</b> A detailed description of the vulnerability, including the affected component or feature.</li>
+        <li><b>Steps to Reproduce:</b> Provide step-by-step instructions on how to reproduce the vulnerability.</li>
+        <li><b>Impact:</b> Explain the potential impact or risks associated with the vulnerability.</li>
+        <li><b>Your Contact Information:</b> Include your name and contact details (email or phone) so that we can get in touch with you regarding the report.</li>
+        </ol>
+        <h2 className='title'>Guidelines</h2>
+        <p className='para'>We have a few important guidelines to ensure a responsible and efficient vulnerability disclosure process:</p>
         <ul className='para'>
-        <li>You agree to use Worko for lawful purposes and in compliance with all applicable laws and regulations.</li>
-        <li>You may not engage in any behavior that disrupts or interferes with the operation of Worko.</li>
-        <li>We reserve the right to suspend or terminate your access to Worko for violations of these terms.</li>
+        <li><b>Do Not Share Publicly:</b> Please do not disclose the vulnerability publicly until it has been resolved and you have received our confirmation.</li>
+        <li><b>Do Not Exploit:</b> Do not attempt to exploit the vulnerability beyond what is necessary for your proof of concept.</li>
+        <li><b>Respect Privacy:</b> Do not access, modify, or delete data that does not belong to you.</li>
         </ul>
-        <h2 className='title'>Privacy Policy</h2>
-        <p className='para'>Your use of Worko is also governed by our Privacy Policy, which outlines how we collect, use, and protect your personal information. You can review our Privacy Policy [here](insert link).</p>
-        <h2 className='title'>User Responsibilities</h2>
+        <h2 className='title'>What to Expect</h2>
         <ul className='para'>
-        <li>You are responsible for the security of your Worko account and any activities that occur under your account.</li>
-        <li>You may not share your Worko login credentials or access another user's account.</li>
-        <li>You may not use Worko in a manner that infringes upon the rights of others or violates any applicable laws.</li>
+          <li>We will acknowledge the receipt of your report and work to confirm the vulnerability.</li>
+          <li>Our security team will assess the report, determine its severity, and develop a plan for remediation.</li>
+          <li>Once the vulnerability is resolved, we will notify you and provide an opportunity for you to confirm the fix.</li>
+          <li>Depending on the nature and impact of the vulnerability, you may be eligible for a security researcher recognition or reward, subject to our discretion.</li>
         </ul>
-        <h2 className='title'>Intellectual Property</h2>
-        <p className='para'>Worko contains proprietary content, trademarks, and intellectual property that are protected by copyright and other laws. You may not use Worko's content without our explicit consent.</p>
-        <h2 className='title'>Third-party Links</h2>
-        <p className='para'>Worko may contain links to third-party websites or services not operated by us. We are not responsible for the content, privacy practices, or terms of such third-party sites.</p>
-        <h2 className='title'>Termination</h2>
-        <p className='para'>We reserve the right to terminate or suspend your access to Worko without prior notice for any violation of these terms or for any other reason we deem appropriate.</p>
-        <h2 className='title'>Disclaimers</h2>
-        <ul className='para'>
-          <li>Worko is provided "as is" without warranties of any kind, whether expressed or implied.</li>
-          <li>We are not liable for any damages or losses resulting from your use of Worko.</li>
-        </ul>
-        <h2 className='title'>Governing Law</h2>
-        <p className='para'>These Terms of Service are governed by the laws of Worko.</p>
-        <h2 className='title'>Changes to This Agreement</h2>
-        <p className='para'>We may update these Terms of Service from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will post the updated Terms of Service on our website.</p>
+        <h2 className='title'>Legal</h2>
+        <p className='para'>Worko is committed to abiding by relevant laws and regulations concerning security vulnerability disclosure. We will not pursue legal action against security researchers who report vulnerabilities responsibly.</p>
+        <p className='para'>Please note that attempting to exploit or disclose vulnerabilities irresponsibly may result in legal action.</p>
+        <p className='para'>Thank you for helping us keep Worko a secure platform for our users.</p>
       </div>
       
       
